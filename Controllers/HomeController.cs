ca0b4ed4845674f5cfa24febc9d30161cb95144d
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2023-11-28 20:54:16 -0500

    popup modal window for contact created

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using FavGames.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Mail;
+using System.Net;
 
 namespace FavGames.Controllers
 {
@@ -14,6 +16,48 @@ namespace FavGames.Controllers
         {
             _logger = logger;
         }
+
+        [HttpPost]
+        public IActionResult Contact(ContactFormModel contactForm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (var smtpClient = new SmtpClient())
+                    {
+                        var mailMessage = new MailMessage
+                        {
+                            From = new MailAddress("your-email@example.com"),
+                            Subject = "New Contact Form Submission",
+                            Body = $"Name: {contactForm.Name}\nEmail: {contactForm.Email}\nMessage: {contactForm.Message}",
+                            IsBodyHtml = false, // Set to true if you use HTML in your message
+                        };
+                        mailMessage.To.Add("recipient-email@example.com");
+
+                        smtpClient.Host = "your-smtp-server"; // Set your SMTP server here
+                        smtpClient.Port = 587; // Set your SMTP port here (587 is common for TLS)
+                        smtpClient.EnableSsl = true;
+                        smtpClient.Credentials = new NetworkCredential("smtp-username", "smtp-password"); // Set your SMTP credentials here
+
+                        smtpClient.Send(mailMessage);
+                    }
+
+                    TempData["Message"] = "Your message has been sent successfully!";
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error sending email from contact form.");
+                    // Handle the exception, log the error, and inform the user.
+                    ModelState.AddModelError("", "There was an error sending your message. Please try again later.");
+                }
+            }
+
+            // If we got this far, something failed; redisplay the form
+            return View(contactForm);
+        }
+
         public IActionResult Index()
         {
             return View(new ContactFormModel());
